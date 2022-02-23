6c38b1ed8dfb2aaf236a141dccad66556330a451
Upshur06 <mupshur2005@gmail.com>
2022-02-23 03:02:36 -0500

    header and footer

--- dvrpc_map_project/src/App.js
+++ dvrpc_map_project/src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Getting started 
+      <header className="App-header">
+        <h1> Indicators of Potential Disadvantage (IPD)</h1>
+      </header>
+
+      <footer>
+        <p>
+          data provided by DVRPC, 2019 Indicators of PotentialDisadvantage and links to: 
+        
+        <a
+          className="App-link"
+          href="https://dvrpc-dvrpcgis.opendata.arcgis.com/datasets/greater- philadelphia-tract-level-indicators-of-potential-disadvantage-ipd-2019/explore"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           https://dvrpc-dvrpcgis.opendata.arcgis.com/datasets/greater- philadelphia-tract-level-indicators-of-potential-disadvantage-ipd-2019/explore
+        </a>
+        </p>
+      </footer>
     </div>
   );
 }
