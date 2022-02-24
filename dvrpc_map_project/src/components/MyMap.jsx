60aa3d03e4937d00875f3f117ce47a87d825532a
Upshur06 <mupshur2005@gmail.com>
2022-02-24 04:47:22 -0500

    centered map viewport

--- dvrpc_map_project/src/components/MyMap.jsx
+++ dvrpc_map_project/src/components/MyMap.jsx
@@ -2,57 +2,17 @@ import React, { useEffect} from 'react'
 import { Map, GeoJSON } from 'react-leaflet'
 import mapData from './../data/map.json'
 import 'leaflet/dist/leaflet.css'
-
-// class MyMap extends Component {
-//     state = {};
-
-//     componentDidMount(){
-//         console.log(mapData)
-//     }
-
-//     render () {
-//         return (
-//             <div>
-//                 <h1>My Map</h1>
-//                 <div className='mapBorder'>
-//                  <Map style={{ height: '80vh' }} zoom={10} center={[40.05, -75]}>
-//                     <GeoJSON data={mapData.features} />
-//                 </Map>
-//              </div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default MyMap
-
-
-// export const MyMap = () => {
-
-
-//     useEffect(() => {
-//         console.log(mapData)
-//     },[])
-
-//     return (
-//         <div>
-//             <h1>My Map</h1>
-//             <div className='mapBorder'>
-//                 <Map style={{ height: '80vh' }} zoom={10} center={[40.05, -75]}>
-//                     <GeoJSON data={mapData.features} />
-//                 </Map>
-//             </div>
-//         </div>
-//     )
-// }
-
+import './../css/MyMap.css'
 
 export default function MyMap() {
+        useEffect(() => {
+            console.log(mapData)
+        },[])
   return (
     <div>
         <h1>My Map</h1>
              <div className='mapBorder'>
-                 <Map style={{ height: '80vh' }} zoom={10} center={[40.05, -75.18]}>
+                 <Map style={{ height: '80vh', width: '90vw', border: '2px solid black' }} zoom={10} center={[40.05, -75.1]}>
                      <GeoJSON data={mapData.features} />
                  </Map>
             </div>
