9a8aebe406a9873acff3447bfed48bf67d48b3aa
Upshur06 <mupshur2005@gmail.com>
2022-02-24 19:07:37 -0500

    fill area with random colors

--- dvrpc_map_project/src/components/MyMap.jsx
+++ dvrpc_map_project/src/components/MyMap.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect} from 'react'
+import React from 'react'
 import { Map, GeoJSON } from 'react-leaflet'
 import mapData from './../data/map.json'
 import 'leaflet/dist/leaflet.css'
 import './../css/MyMap.css'
 
 export default function MyMap() {
-        // const countryStyle = {
-        //     fillColor: 'red'
-        // }
+
+    const colors = ['#2477A3', '#77B2D1', '#214C63', '#22A0E2', '#1199E0', '#033550', '#02537D', '#02537D',         
+                    '#DFF2FD', '#DFF2FD', '#66A8CB', '#56A4CC', '#3393C6', '#3D87AD', '#BFD2DD', '#092C3F', '#ECEFF0', '#0B6EA1', '#0B6EA1', '#3589B5', '#567585', '#567585', '#78B7D8', '#4C7285', '#88959C', '#5297BB', '#62B1DA', '#0F89C9', '#17658E', '#5190B0','#4A5A62', '#0173AF', '#ACBAC1', '#81C5E9', '#2175B5', '#0A4D80', '#1997F8']
 
         const onEachArea = (area, layer) =>{
             const area_IPD = `score: ${area.properties.ipd_score}`
@@ -20,17 +20,7 @@ export default function MyMap() {
             const area_OA = `score: ${area.properties.oa_score}`
             const area_RM = `score: ${area.properties.rm_score}`
             const area_Y = `score: ${area.properties.y_score}`
-            // console.log(`Property: ${area_IPD}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_D} Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_EM}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_F}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_FB}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_LEP}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_LI}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_OA}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_RM}, Display Name: Indicators of Potential Disadvantage
-            // Property: ${area_Y}, Display Name: Indicators of Potential Disadvantage
-            // `)
+
             layer.bindPopup(`Property: ${area_IPD}, Display Name: Indicators of Potential Disadvantage<br>
             Property: ${area_D} Display Name: Indicators of Potential Disadvantage<br>
             Property: ${area_EM}, Display Name: Indicators of Potential Disadvantage<br>
@@ -42,6 +32,9 @@ export default function MyMap() {
             Property: ${area_RM}, Display Name: Indicators of Potential Disadvantage<br>
             Property: ${area_Y}, Display Name: Indicators of Potential Disadvantage
             `)
+
+            const colorIndex = Math.floor(Math.random() * colors.length)
+            layer.options.fillColor = colors[colorIndex];
 
             const onTargetMouseOver = (e) => {
                 e.target.setStyle({
@@ -60,14 +53,6 @@ export default function MyMap() {
                 mouseout: offTargetMouseOut
             })
         }
-
-        useEffect(() => {
-            // console.log(mapData)
-        },[])
-
-        // onEachCountry = (country, layer) => {
-        //     console.log(country)
-        // }
 
   return (
     <div>
